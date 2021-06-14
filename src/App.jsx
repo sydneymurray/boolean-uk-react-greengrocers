9c8a40518752d6539e6cdbaa4f6a6465ea952f49
sydneymurray <sydney_i_murray@yahoo.co.uk>
2021-06-15 00:19:12 +0100

    JSON ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,115 @@
-import { useState } from 'react'
-import products from './data/products.js'
-import ImportedCart from './data/cart.js'
+import { useEffect, useState } from 'react'
+//import products from './data/products.js'
+//import ImportedCart from './data/cart.js'
 import ProductsSection from "./components/ProductsSection.js"
 import CartSection from "./components/CartSection.js"
 import "./styles/index.css";
 
 export default function App() {
-    let [cart, setCart] = useState(ImportedCart)
+    let [products, setProducts] = useState([])
+    let [cart, setCart] = useState([])
 
-    function AddToCart(storeItem){
-        let index = cart.findIndex(cartItem => storeItem.id === cartItem.id) 
-        if (index === -1) {
-            storeItem = {...storeItem, quantity: 1}    
-            cart = [...cart, storeItem]
-        }
-        else         
-            cart = cart.map(cartItem => {
-                if (cartItem.id === storeItem.id) 
-                    cartItem = {...cartItem, quantity: ++cartItem.quantity}
-                return cartItem
-            }) 
-        setCart(cart)
+    useEffect(() => {retrieveJsonDataArrays()}, [])
+
+    function retrieveJsonDataArrays() {
+        retrieveProducts()
+        retrieveCart()
+    }
+
+    function retrieveProducts() {
+        return fetch("http://localhost:3100/products")
+            .then(function (promise) {
+                return promise.json()})
+            .then(function (productArray) {
+                setProducts(productArray)})
     }
     
-    function RemoveFromCart(item){
-        if (item.quantity === 1)
-            cart = cart.filter(cartItem => cartItem.id !== item.id) 
-        else
-            cart = cart.map(cartItem => {
-                if (cartItem.id === item.id) 
-                    cartItem = {...cartItem, quantity: --cartItem.quantity}
-                return cartItem
+    function retrieveCart() {
+        return fetch("http://localhost:3100/cart")
+            .then(function (promise) {
+                return promise.json()})
+            .then(function (cartArray) {
+                setCart(cartArray)})
+    }
+
+    function AddToCart(storeItem) {
+        let index = cart.findIndex(cartItem => storeItem.id === cartItem.id)
+        if (index === -1) {
+            storeItem = { ...storeItem, quantity: 1 }
+            return fetch(`http://localhost:3100/cart`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'Application/json' },
+                body: JSON.stringify(storeItem)})
+                .then(response => {
+                if (response.ok){
+                    cart = [...cart, storeItem]
+                    console.log("CARROT OK",cart)
+                    setCart(cart)
+                }
             })
-        setCart(cart)
+        }
+        else{
+            return fetch(`http://localhost:3100/cart/${storeItem.id}`,{
+                method:'PATCH',
+                headers:{'Content-Type': 'Application/json'},
+                body: JSON.stringify({quantity: storeItem.quantity + 1})})
+                .then(response => {
+                if (response.ok){
+                    cart = cart.map(cartItem => {
+                        if (cartItem.id === storeItem.id) 
+                            cartItem = {...cartItem, quantity: ++cartItem.quantity}
+                        return cartItem 
+                    })
+                    setCart(cart)
+                }
+                })
+            }
+        }
+    
+    function RemoveFromCart(item) {
+        if (item.quantity === 1){
+            fetch(`http://localhost:3100/cart/${item.id}`,{method:'DELETE'})
+            .then(response => {
+                if (response.ok){
+                    cart = cart.filter(cartItem => cartItem.id !== item.id)
+                    setCart(cart)
+                }
+            })
+        }
+        else{
+            fetch(`http://localhost:3100/cart/${item.id}`,{
+                method:'PATCH',
+                headers:{'Content-Type': 'Application/json'},
+                body: JSON.stringify({quantity: item.quantity - 1})
+            }).then(response => {
+                if (response.ok){
+                    cart = cart.map(cartItem => {
+                        if (cartItem.id === item.id)
+                            cartItem = { ...cartItem, quantity: --cartItem.quantity }
+                        return cartItem
+                    })
+                    console.log(cart)
+                    setCart(cart)
+                }
+            })
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+        }
     }
 
     return <div className="App">
-        <ProductsSection products={products} AddToCart={AddToCart}/>
-        <CartSection cart={cart} AddToCart={AddToCart} 
-            RemoveFromCart={RemoveFromCart}/>
+        <ProductsSection products={products} AddToCart={AddToCart} />
+        <CartSection cart={cart} AddToCart={AddToCart}
+            RemoveFromCart={RemoveFromCart} />
     </div>;
 }
